06d819de7df9595e73e3067e422193436574e76d
deionnle <render48@yandex.ru>
2024-09-21 18:15:15 +0300

    add Queue.java

---
+++ Queue.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Queue<T> {
+    private ArrayList<T> queue;
+    private int count;
+
+    public Queue() {
+    queue = new ArrayList<T>();
+    count = 0;
+    }
+
+    public void enqueue(T item) {
+        queue.add(count ++,item);
+    }
+
+    public T dequeue() {
+        if (queue.isEmpty()) return null;
+        T que = queue.get(0);
+        queue.remove(0);
+        count--;
+        return que;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public void turn(int n) {
+        for (int i = 0; i < n; i ++) {
+            T temp = dequeue();
+            enqueue(temp);
+        }
+    }
+}
+
+
