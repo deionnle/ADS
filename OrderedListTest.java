6c99bb7c9a158b5a93c19ee8254ea8ef0275cf19
deionnle <render48@yandex.ru>
2024-11-01 10:25:13 +0300

    added new tests

--- OrderedListTest.java
+++ OrderedListTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderedListTest {
@@ -86,5 +89,116 @@ public class OrderedListTest {
             orderedList.add(i);
         }
         assertEquals(null,(orderedList.find(10)));
+    }
+
+    @Test
+    public void RemoveDuplicatesTest() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);list.add(2);list.add(2);
+        list.add(3);list.add(3);list.add(3);
+        list.add(4);
+
+        list.removeDuplicates();
+
+        assertEquals(4, list.count());
+        assertEquals(1, list.head.value);
+        assertEquals(4, list.tail.value);
+
+        assertEquals(1, list.head.value);
+        assertEquals(2, list.head.next.value);
+        assertEquals(3, list.head.next.next.value);
+        assertEquals(4, list.head.next.next.next.value);
+    }
+
+    @Test
+    public void MergeAscendingTest() {
+        OrderedList<Integer> list1 = new OrderedList<>(true);
+        OrderedList<Integer> list2 = new OrderedList<>(true);
+
+        list1.add(1);list1.add(3);list1.add(5);
+        list2.add(2);list2.add(4);list2.add(6);
+
+        OrderedList<Integer> mergedList = OrderedList.merge(list1, list2, true);
+        Integer[] expected = {1, 2, 3, 4, 5, 6};
+
+        Node<Integer> mergedNode = mergedList.head;
+        for (int i = 0; i < expected.length; i++) {
+            assertNotNull(mergedNode);
+            assertEquals(expected[i], mergedNode.value);
+            mergedNode = mergedNode.next;
+        }
+        assertNull(mergedNode);
+    }
+
+    @Test
+    public void MergeDescendingTest() {
+        OrderedList<Integer> list1 = new OrderedList<>(false);
+        OrderedList<Integer> list2 = new OrderedList<>(false);
+
+        list1.add(5);list1.add(3);list1.add(1);
+        list2.add(6);list2.add(4);list2.add(2);
+
+        OrderedList<Integer> mergedList = OrderedList.merge(list1, list2, false);
+        Integer[] expected = {6, 5, 4, 3, 2, 1};
+
+        ArrayList<Node<Integer>> mergedNodes = mergedList.getAll();
+
+        assertEquals(expected.length, mergedNodes.size());
+
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], mergedNodes.get(i).value);
+        }
+    }
+
+    @Test
+    void SubListTest() {
+        OrderedList<Integer> List = new OrderedList<>(true);
+        OrderedList<Integer> subList = new OrderedList<>(true);
+
+        List.add(1);List.add(2);List.add(3);List.add(4);
+
+        subList.add(1);
+        subList.add(2);
+        assertTrue(List.SubList(subList));
+
+        subList.clear(true);
+
+        subList.add(3);
+        subList.add(4);
+        assertTrue(List.SubList(subList));
+
+        subList.clear(true);
+        assertTrue(List.SubList(subList));
+
+        subList.add(2);
+        subList.add(4);
+
+        assertFalse(List.SubList(subList));
+
+        List.clear(true);
+        assertFalse(List.SubList(subList));
+    }
+
+    @Test
+    public void FrequentTest() {
+        OrderedList<Integer> list = new OrderedList<>(true);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(1);
+        list.add(3);
+        list.add(2);
+        list.add(3);
+        assertEquals(3, list.findFrequentValue());
+    }
+
+    @Test
+    public void FindIndexTest() {
+        OrderedList<Integer> list = new OrderedList<>(false);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        assertEquals(1, list.findIndex(2));
+        assertEquals(-1, list.findIndex(4));
     }
 }
