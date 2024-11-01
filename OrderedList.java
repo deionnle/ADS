aaa9366793ce4d7dc188b3c1693c8a795bbcfb17
deionnle <render48@yandex.ru>
2024-11-01 10:24:54 +0300

    added new methods

--- OrderedList.java
+++ OrderedList.java
@@ -161,6 +161,149 @@ public class OrderedList<T>
         }
         return r;
     }
+
+    public void removeDuplicates() {
+        if (this.head == null) return;
+
+        Node<T> current = this.head;
+        while (current != null && current.next != null) {
+            if (compare(current.value, current.next.value) == 0) {
+                Node<T> duplicate = current.next;
+                current.next = duplicate.next;
+                if (duplicate.next != null) {
+                    duplicate.next.prev = current;
+                } else {
+                    this.tail = current;
+                }
+            } else {
+                current = current.next;
+            }
+        }
+    }
+
+    public static <T> OrderedList<T> merge(OrderedList<T> list1, OrderedList<T> list2, boolean ascending) {
+        OrderedList<T> reverseList1;
+        OrderedList<T> reverseList2;
+
+        if (list1._ascending != ascending) {
+            reverseList1 = list1.revers();
+        } else {
+            reverseList1 = list1;
+        }
+
+        if (list2._ascending != ascending) {
+            reverseList2 = list2.revers();
+        } else {
+            reverseList2 = list2;
+        }
+
+        OrderedList<T> reverse = new OrderedList<>(ascending);
+
+        Node<T> node1 = reverseList1.head;
+        Node<T> node2 = reverseList2.head;
+
+        while (node1 != null && node2 != null) {
+            if ((list1.compare(node1.value, node2.value) <= 0 && ascending) ||
+                    (list1.compare(node1.value, node2.value) >= 0 && !ascending)) {
+                reverse.add(node1.value);
+                node1 = node1.next;
+            } else {
+                reverse.add(node2.value);
+                node2 = node2.next;
+            }
+        }
+
+        while (node1 != null) {
+            reverse.add(node1.value);
+            node1 = node1.next;
+        }
+
+        while (node2 != null) {
+            reverse.add(node2.value);
+            node2 = node2.next;
+        }
+        return reverse;
+    }
+
+    public OrderedList<T> revers() {
+        OrderedList<T> reversedList = new OrderedList<>(!_ascending);
+
+        Node<T> node = this.tail;
+        while (node != null) {
+            reversedList.add(node.value);
+            node = node.prev;
+        }
+        return reversedList;
+    }
+
+    public boolean SubList(OrderedList<T> subList) {
+        if (subList.head == null) return true;
+        if (this.head == null) return false;
+
+        Node<T> current = this.head;
+
+        while (current != null) {
+            if (compare(current.value, subList.head.value) == 0 && checkSubList(current, subList.head)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    private boolean checkSubList(Node<T> Node, Node<T> subNode) {
+        while (subNode != null) {
+            if (Node == null || compare(Node.value, subNode.value) != 0) {
+                return false;
+            }
+            Node = Node.next;
+            subNode = subNode.next;
+        }
+        return true;
+    }
+
+    public T findFrequentValue() {
+        if (head == null) return null;
+
+        HashMap<T, Integer> valuesMap = new HashMap<>();
+        T Frequent = null;
+        int maxCount = 0;
+
+        for (Node<T> current = head; current != null; current = current.next) {
+            Integer count = valuesMap.get(current.value);
+            if (count == null) {
+                count = 0;
+            }
+            count++;
+            valuesMap.put(current.value, count);
+
+            if (count > maxCount) {
+                maxCount = count;
+                Frequent = current.value;
+            }
+        }
+        return Frequent;
+    }
+
+    public int findIndex(T value) {
+        if (this.head == null) return -1;
+
+        int index = 0;
+        Node<T> current = this.head;
+
+        while (current != null) {
+            int val = compare(value, current.value);
+
+            if (val == 0) {
+                return index;
+            } else if ((_ascending && val < 0) || (!_ascending && val > 0)) {
+                return -1;
+            }
+            current = current.next;
+            index ++;
+        }
+        return -1;
+    }
 }
 
 
