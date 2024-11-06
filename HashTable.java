82e1d63dc7c3a82012ad08fa6c3d364154020e32
deionnle <render48@yandex.ru>
2024-11-06 17:07:27 +0300

    added new methods

--- HashTable.java
+++ HashTable.java
@@ -1,22 +1,30 @@
+import java.util.Random;
+
 public class HashTable
 {
     public int size;
     public int step;
+    private int count;
     public String [] slots;
+    private int salt;
 
     public HashTable(int sz, int stp)
     {
         size = sz;
         step = stp;
+        count = 0;
         slots = new String[size];
         for(int i=0; i<size; i++) slots[i] = null;
+
+        Random rand = new Random();
+        salt = rand.nextInt(size);
     }
 
     public int hashFun(String value)
     {
-        int hash = 0;
+        int hash = salt;
         for (int i = 0; i < value.length(); i++) {
-            hash += value.charAt(i);
+            hash = hash * 31 + value.charAt(i);
         }
         return Math.abs(hash % size);
     }
@@ -37,12 +45,16 @@ public class HashTable
 
     public int put(String value)
     {
-        int index = seekSlot(value);
-        if (index == -1) {
-            return -1;
+        if (count == size) {
+            resize();
         }
-        slots[index] = value;
-        return index;
+        int index = seekSlot(value);
+        if (index != -1) {
+            slots[index] = value;
+            count ++;
+            return index;
+        }
+        return -1;
     }
 
     public int find(String value)
@@ -60,6 +72,19 @@ public class HashTable
             }
         }
         return -1;
+    }
+
+    private void resize() {
+        String[] oldSlots = slots;
+        size *= 2;
+        slots = new String[size];
+        count = 0;
+
+        for (String slot : oldSlots) {
+            if (slot != null) {
+                put(slot);
+            }
+        }
     }
 }
 
